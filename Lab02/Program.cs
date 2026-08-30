e45ce83b133ed7b1a1da0d6d0e263d2c0ba415f4
Arnuparp Anuraksiam - 6609650756 <108282109+bas1ooo@users.noreply.github.com>
2026-08-30 16:58:47 +0700

    update partB

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,8 +1,8 @@
 ﻿/*
- * Student ID :1690704588
- * Name       :Lab02
- * Section    :129D
- * No.        :NA
+ * Student ID : 1690704588
+ * Name       : อานุภาพ อนุรักษ์สยาม
+ * Section    : 129D
+ * No.        : NA
  * Course     : GI113 Computer Programming (GI)
  */
 
@@ -51,39 +51,31 @@ namespace Lab02
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent: {hpPercent}%");
 
-
             // ==================== PART B ====================
 
-            // Character 1
             string name1 = "Knight";
             int hp1 = 500;
             float attack1 = 75.5f;
             char rank1 = 'A';
             bool isAlive1 = true;
 
-            // Character 2
             string name2 = "Mage";
             int hp2 = 300;
             double mana2 = 250.75;
             char rank2 = 'S';
             bool isAlive2 = true;
 
-            // Character 3
             string name3 = "Archer";
             int hp3 = 350;
             float speed3 = 8.5f;
             double accuracy3 = 95.25;
             bool isAlive3 = true;
 
-            // Character 4
             string name4 = "Dragon";
             int hp4 = 1000;
             float attack4 = 150.5f;
             char rank4 = 'S';
             bool isAlive4 = true;
-
-
-            Console.WriteLine();
 
             Console.WriteLine("===== CHARACTER 1 =====");
             Console.WriteLine($"Name: {name1}");
